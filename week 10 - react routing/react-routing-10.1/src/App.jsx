070e3937f3e882fc5a0fccc042044ffdbc15d04b
Kunal-Khare <kunalkhare47@gmail.com>
2025-03-26 23:33:09 +0530

    outlet,layout,useref

--- week 10 - react routing/react-routing-10.1/src/App.jsx
+++ week 10 - react routing/react-routing-10.1/src/App.jsx
@@ -1,6 +1,12 @@
-
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  Outlet,
+} from "react-router-dom";
 
 function App() {
   return (
@@ -8,8 +14,7 @@ function App() {
     // we are using link and navlink for routed links
     // we cannot use <Link/> outside the browser router, we have to use the link inside BrowserRouter
     <div>
-    
-     {/*
+      {/*
       <a href="/">Allen</a>
 
       <a href="/neet/online-coaching-class-11">Class 11</a>
@@ -18,21 +23,42 @@ function App() {
       */}
 
       <BrowserRouter>
-      <Link to="/">Allen</Link>
-      <Link to="/neet/online-coaching-class-11" >Class 11</Link>
-      <Link to="/neet/online-coaching-class-12">Class 12</Link>
         <Routes>
-          <Route
-            path="/neet/online-coaching-class-11"
-            element={<Class11Program />}
-          ></Route>
-          <Route
-            path="/neet/online-coaching-class-12"
-            element={<Class12Program />}
-          ></Route>
-          <Route path="/" element={<Landing />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route
+              path="/neet/online-coaching-class-11"
+              element={<Class11Program />}
+            ></Route>
+            <Route
+              path="/neet/online-coaching-class-12"
+              element={<Class12Program />}
+            ></Route>
+            <Route path="/" element={<Landing />}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <div style={{ height: "100vh" }}>
+      <Header />
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Outlet />
+      </div>
+      Footer | ContactUs
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div>
+      <Link to="/">Allen</Link>
+      <Link to="/neet/online-coaching-class-11">Class 11</Link>
+      <Link to="/neet/online-coaching-class-12">Class 12</Link>
     </div>
   );
 }
@@ -55,9 +81,18 @@ function Class11Program() {
 }
 
 function Class12Program() {
+  // using useNavigate() hook--
+
+  const navigate = useNavigate();
+
+  function redirector() {
+    navigate("/");
+  }
+
   return (
     <div>
       <h1>Class 12 Program</h1>
+      <button onClick={redirector}>Go back to home page</button>
     </div>
   );
 }
