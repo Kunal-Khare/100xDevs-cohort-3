a0a2a2569854adebbebb903ab8a7924c64b39370
Kunal-Khare <kunalkhare47@gmail.com>
2025-03-31 23:11:07 +0530

    bulb-problem

--- week 10 - react routing/react-contextAPI-10.2/state-experiments/src/App.jsx
+++ week 10 - react routing/react-contextAPI-10.2/state-experiments/src/App.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import bulbOnImage from "./assets/on.png";
+import bulbOffImage from "./assets/off.png";
 
 function App() {
   return (
-    <div>hi there</div>
-  )
+    <div>
+      <LightBulb />
+    </div>
+  );
 }
 
-export default App
+function LightBulb() {
+  const [bulbOn, setBulbOn] = useState(false);
+  return (
+    <div>
+      <BulbState bulbOn={bulbOn} />
+      <SwitchBulbState bulbOn={bulbOn} setBulbOn={setBulbOn} />
+    </div>
+  );
+}
+
+function BulbState({ bulbOn }) {
+  return (
+    <div>
+      <img
+        src={bulbOn ? bulbOnImage : bulbOffImage}
+        alt={bulbOn ? "Bulb ON" : "Bulb OFF"}
+        style={{ width: "100px", height: "auto" }}
+      />
+    </div>
+  );
+}
+
+function SwitchBulbState({ bulbOn, setBulbOn }) {
+  function toggle() {
+    setBulbOn(!bulbOn);
+  }
+  return (
+    <div>
+      <button onClick={toggle}>Toggle the Bulb</button>
+    </div>
+  );
+}
+
+export default App;
