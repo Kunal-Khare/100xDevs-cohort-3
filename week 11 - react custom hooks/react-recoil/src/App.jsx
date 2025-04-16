a83afe02c041d07314a7333ac99c30c0f842bc0a
Kunal-Khare <kunalkhare47@gmail.com>
2025-04-16 22:51:33 +0530

    initial commit

--- week 11 - react custom hooks/react-recoil/src/App.jsx
+++ week 11 - react custom hooks/react-recoil/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react'
+
+
+//custom hooks
+function useCounter() {
+  const [count, setCount] = useState(0);
+
+  function increaseCount(){
+    setCount(count + 1);
+  }
+  return {
+    count : count,
+    increaseCount: increaseCount
+  }
+}
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const {count, increaseCount} = useCounter();
+  return ( 
+    <div>
+      <button onClick={increaseCount}>
+        Increase {count}
+      </button>
+    </div>
   )
 }
 
