eef6ffecf2044eb2e71b3a5ff1416d41551706cf
Kunal-Khare <kunalkhare47@gmail.com>
2025-05-13 23:22:37 +0530

    js-revisit

---
+++ js-revisit/basic_assignment.js
@@ -0,0 +1,19 @@
+// variables in js - var, let, const
+
+/*
+let name = 'dexter';
+const age = 23;
+var isStudent = true;
+let height =  '168cm';
+
+console.log(name, age, isStudent, height);
+*/
+
+// Functions -  functions are reusable blocks of code that perform a specific task.
+
+function greet(name) {
+  return "Hello," + name;
+  //console.log("Hello," + name);
+}
+let message = greet("Dexter");
+console.log(message);
